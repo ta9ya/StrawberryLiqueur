9936dd7001288cc5e75547f889f01e2c3648cae1
uskoo <usk.moo@gmail.com>
2019-04-21 14:29:03 +0900

    main.py json input to url output (tentative)

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
+import json
+import match
+
+json_str = '''
+{"artist":"高垣楓",
+"title":"こいかぜ"}
+'''
+#json_str = input()
+
+json_dict = json.loads(json_str)
+print(json_dict["artist"])
+
+artist = json_dict["artist"]
+title = json_dict["title"]
+
+url = hogehoge(artist, title)
+
+print(url)
