a2a0a4643f8c0811bbd48ba4be257937627596d7
ta9ya <t.iwasa0821@gmail.com>
2019-04-21 14:59:55 +0900

    match.py の外形を作成

--- match.py
+++ match.py
@@ -1,2 +1,37 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
+
+
+class MusicSearch:
+	def __init__(self, _music_data):
+		self.music_data = _music_data
+		self.keys = ['artist', 'title', 'character_voice']
+
+	def _calc_similarity(self, query_word, key):
+		perplexity = 0
+
+		return perplexity
+
+	def search_data(self, query_dict):
+		"""
+		果物の値段を取得する。
+
+		Parameters
+		----------
+		query_dict : dict
+			{
+				artist: str
+					曲を歌ってるアイドル名
+				title : str
+					曲のタイトル
+				character_voice : str
+					声優名
+			}
+
+		Returns
+		----------
+		music_data : dict
+		"""
+
+		perplexities = [self._calc_similarity(query_dict[key], key) for key in self.keys]
+
