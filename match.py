2fa13c822ea7d4fc483c74e735e678d545fe295e
ta9ya <t.iwasa0821@gmail.com>
2019-04-21 15:59:12 +0900

    レーベンシュタイン距離を計算

--- match.py
+++ match.py
@@ -2,15 +2,29 @@
 # -*- encoding:utf-8 -*-
 
 
-class MusicSearch:
+from levenshtein import Levenshtein
+
+
+class MusicSearch(Levenshtein):
 	def __init__(self, _music_data):
+		super(Levenshtein, self).__init__(self)
 		self.music_data = _music_data
 		self.keys = ['artist', 'title', 'character_voice']
 
-	def _calc_similarity(self, query_word, key):
-		perplexity = 0
+	def _calc_similarity(self, query_word, search_word):
+		# leven = Levenshtein(query_word, search_word)
 
-		return perplexity
+		long_word_length = len(query_word)  if len(query_word) > len(search_word) else len(search_word)
+
+		self.str1 = query_word
+		self.str2 = search_word
+		distance = self.initArray(self.str1, self.str2)
+		dist = self.editDist(self.str1, self.str2, distance)
+		return dist / long_word_length
+
+	def _calc_perplexity(self, perplexities):
+		total_perplexity = sum(perplexities) / len(perplexities)
+		return total_perplexity
 
 	def search_data(self, query_dict):
 		"""
@@ -33,5 +47,25 @@ class MusicSearch:
 		music_data : dict
 		"""
 
-		perplexities = [self._calc_similarity(query_dict[key], key) for key in self.keys]
+		music_list = list()
 
+		for data in self.music_data:
+			flag = True
+			perplexities = []
+
+			for key in self.keys:
+				perplexity = self._calc_similarity(query_dict[key], search_word)
+
+				if perplexity > 0:
+					perplexities.append(perplexity)
+				else:
+					flag = False
+					break
+
+			if flag:
+				total_perplexity = self._calc_perplexity(perplexities)
+				music_list.append([data, total_perplexity])
+
+		music_data = max(music_list, key=lambda x:x[1])[0]
+
+		return music_data
